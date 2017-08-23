9dfa659745b76a36e3e4b8ef0e2cf9e4c58f45f4
Eduardo <e.aboy@sinteac.es>
2017-08-23 22:11:44 +0200

    Adds dotdotdot module and applies it to article-intro

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 window.$ = window.jQuery = require('jquery');
+require('dotdotdot');
 
 import Animations from './animations.js';
 import Header from './header.js';
 
 const header = new Header(Animations);
 header.init();
+
+$(".article-intro").dotdotdot({watch:'window'});
