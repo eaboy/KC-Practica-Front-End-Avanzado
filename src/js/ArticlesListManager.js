3fde3c1d9213e7e583b22759fd986b32182b1a44
Eduardo <e.aboy@sinteac.es>
2017-08-27 19:15:55 +0200

    Updates ArticlesListManager.js and main.js

--- src/js/ArticlesListManager.js
+++ src/js/ArticlesListManager.js
@@ -24,11 +24,41 @@ export default class ArticlesListManager{
     }
 
     renderArticles(articles){
-        console.log('renedering',articles);
+        let html = '';
+
+        for (let article of articles) {
+            html += this.renderArticle(article);
+        }
+
+        this.uiManager.setIdealHtml(html);
     }
 
     renderArticle(article){
-
+        let photo = article.photo_name;
+        if (photo === '') {
+            photo = 'placeholder-male';
+        }
+        return `<article class="article">
+                    <picture>
+                        <source srcset="./img/${article.image_name}-s.jpg 500w, ./img/${article.image_name}-m.jpg 800w, ./img/${article.image_name}-l.jpg 1100w" media="(min-width: 768px)">
+                        <img src="./img/${article.image_name}-s.jpg" alt="" class="article-image">
+                    </picture>
+                    <h2 class="title">${article.title}</h2>
+                    <p class="article-intro">${article.intro_text}</p>
+                    <div class="article-info">
+                        <div class="article-publish-container">
+                            <img src="./img/${photo}.png" alt="" class="author-photo">
+                            <div class="publish-info">
+                                <h5 class="author-name">${article.author}</h5>
+                                <div class="publish-time">7 min ago</div>
+                            </div>
+                        </div>
+                        <div class="extras">
+                            <div class="article-comments"><i class="fa fa-comment-o" aria-hidden="true"></i><span class="number-comments">${article.comments}</span></div>
+                            <div class="like-icon"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
+                        </div>
+                    </div>
+                </article>`
     }
 
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,19 @@
 window.$ = window.jQuery = require('jquery');
 require('dotdotdot');
 
-import Animations from './animations.js';
-import Header from './header.js';
+import Animations from './animations';
+import Header from './header';
+import APIService from './APIService';
+import UIManager from './UIManager';
+import ArticlesListManager from './ArticlesListManager';
 
 const header = new Header(Animations);
 const animation = new Animations();
+const articlesService = new APIService('/articles');
+const articlesUIManager = new UIManager('.articles-list');
+
+const articlesListManager = new ArticlesListManager(articlesService, articlesUIManager);
+articlesListManager.init();
 
 animation.init();
 header.init();
