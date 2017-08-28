1fd74bc2e01b190e9eccf672b4acf9be1e42d18e
Eduardo <e.aboy@sinteac.es>
2017-08-28 22:50:41 +0200

    Adds some style to publish date and refactors renderArticle function

--- src/js/ArticlesListManager.js
+++ src/js/ArticlesListManager.js
@@ -39,34 +39,17 @@ export default class ArticlesListManager{
     }
 
     renderArticle(article){
-        let photo = article.photo_name;
-        let articleMedia;
-
-        if (photo === '') { // Checks if there is no author photo, if not uses the default placeholder photo
-            photo = 'placeholder-male';
-        }
-        
-        if (article.video_name === ''){ // Checks if there is no video for the article, if not uses the html for images and the image, else uses the html for videos and the video
-            articleMedia = `<picture>
-                                <source srcset="./img/${article.image_name}-s.jpg 500w, ./img/${article.image_name}-m.jpg 800w, ./img/${article.image_name}-l.jpg 1100w" media="(min-width: 768px)">
-                                <img src="./img/${article.image_name}-s.jpg" alt="" class="article-image">
-                            </picture>`;
-        } else {
-            articleMedia = `<video class="video" controls>
-                                <source src="./videos/${article.video_name}.mp4" type="video/mp4">
-                            </video>`
-        }
 
         return `<article class="article">      
-                    ${articleMedia}
+                    ${this.getMediaHtml(article)}
                     <h2 class="title">${article.title}</h2>
                     <p class="article-intro">${article.intro_text}</p>
                     <div class="article-info">
                         <div class="article-publish-container">
-                            <img src="./img/${photo}.png" alt="" class="author-photo">
+                            <img src="./img/${this.getArticlePhoto(article.photo_name)}.png" alt="" class="author-photo">
                             <div class="publish-info">
                                 <h5 class="author-name">${article.author}</h5>
-                                <div class="publish-time">${this.publishedFromNow(article.publish_date)}</div>
+                                <div class="publish-time">Published ${this.publishedFromNow(article.publish_date)}</div>
                             </div>
                         </div>
                         <div class="extras">
@@ -77,16 +60,41 @@ export default class ArticlesListManager{
                 </article>`
     }
 
-    publishedFromNow(date) {
+    publishedFromNow(date) { // Returns the publish date from now if is less than a day ago, the publish day if is less than a week ago otherwise the publish date
 
-        let daysFromPublish = moment().diff(moment(date,'YYYY/MM/DD hh:mm:ss'), 'days');
+        let daysFromPublish = moment().diff(moment(date,'YYYY/MM/DD hh:mm:ss'), 'days'); // Get the number of days from published date
+
         if (daysFromPublish > 6 ) {
-            return moment(date).format('YYYY/MM/DD');
+            return moment(date).format('YYYY/MM/DD'); 
         } else if (daysFromPublish < 1) {
             return moment(date).fromNow();
         } else {
-            return `Last ${moment(date).format('dddd')}`;
+            return `last ${moment(date).format('dddd')}`;
         }
+    }
+
+    getMediaHtml(article) { // Checks if there is no video for the article, if not returns the html for images and the image, else returns the html for videos and the video
+        
+        if (article.video_name === ''){ 
+            return `<picture>
+                                <source srcset="./img/${article.image_name}-s.jpg 500w, ./img/${article.image_name}-m.jpg 800w, ./img/${article.image_name}-l.jpg 1100w" media="(min-width: 768px)">
+                                <img src="./img/${article.image_name}-s.jpg" alt="" class="article-image">
+                            </picture>`;
+        } else {
+            return `<video class="video" controls>
+                                <source src="./videos/${article.video_name}.mp4" type="video/mp4">
+                            </video>`
+        }
+    }
+
+    getArticlePhoto(photo) { // Checks if there is no author photo, if not returns the default placeholder photo
+
+        if (photo === '') {
+            return 'placeholder-male';
+        } else {
+            return photo;
+        }
+
     }
 
 }
